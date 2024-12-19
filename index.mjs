c4a18f24927b52af17ffa363b531ab02f101b0de
Jordan Harband <ljharb@gmail.com>
2024-12-18 22:05:02 -0800

    [Refactor] remove useless ESM implementation

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import callBind from 'call-bind';
-
-import getPolyfill from 'number.prototype.toexponential/polyfill';
-
-export default callBind(getPolyfill());
-
-export { default as getPolyfill } from 'number.prototype.toexponential/polyfill';
-export { default as implementation } from 'number.prototype.toexponential/implementation';
-export { default as shim } from 'number.prototype.toexponential/shim';
